70c31ffc32e0d1199bc8f48234817b87630ed4f5
Satoru Uno <unofamil@outlook.com>
2024-05-13 10:46:59 +0900

    WebAssembly.instantiate を用いて calc を呼び出す処理を追加

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
-WebAssembly.instantiateStreaming(fetch("calc.wasm")).then(
-    (results) => {
-        const { add } = results.instance.exports
-        console.log(add(1, 2))
-    },
-);
+import fs from 'fs';
+const wasmCode = fs.readFileSync('build/calc.wasm');
+
+WebAssembly.instantiate(wasmCode, {})
+    .then(module => {
+        const instance = module.instance;
+        const calc = instance.exports.calc;
+        const sum = calc(2, 3);
+        console.log(sum);
+    })
+    .catch(error => {
+        console.error("Error during instantiation:", error);
+    })
