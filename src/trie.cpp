d081f9491e06109675d948491a7dd90ef63bc8bb
Satoru Uno <unofamil@outlook.com>
2024-05-20 02:59:58 +0900

    src/trie.cpp の修正

--- src/trie.cpp
+++ src/trie.cpp
@@ -2,70 +2,82 @@
 #include <string>
 #include <vector>
 
-extern "C" {
+using namespace std;
 
-    using namespace std;
+typedef struct Node Node;
 
-    typedef struct Node Node;
+struct Node {
+    char c; //ノードが持つ文字
+    vector<Node*> children;
+};
 
-    struct Node {
-        char c; //ノードが持つ文字
-        vector<Node*> children;
-    };
+class Trie {
 
-    class Trie {
+public:
 
-    public:
+    Node root;
 
-        Node root;
+    Trie() {
+        // コンストラクタ
+        // cout << "Make Trie." << endl;
+        return ;
+    }
 
-        Trie(){
-            // コンストラクタ
-            // cout << "Make Trie." << endl;
-            return ;
-        }
-
-        void insert(string str){ // 文字列 str を登録する
-            Node *ptr = &root; // 現在のノード
-            int pos = 0; // 文字列 str 上の位置
-            while(pos < str.size()){
-                bool isExistsChild = false;
-                for(int i = 0;i < (ptr->children).size(); i++){
-                if(ptr->children[i]->c == str[pos]){
+    void insert(string str) { // 文字列 str を登録する
+        Node *ptr = &root; // 現在のノード
+        int pos = 0; // 文字列 str 上の位置
+        while (pos < str.size()) {
+            bool isExistsChild = false;
+            for (int i = 0;i < (ptr->children).size(); i++) {
+                if (ptr->children[i]->c == str[pos]) {
                     isExistsChild = true;
                     ptr = ptr->children[i];
                     break;
                 }
-                }
-                if(!isExistsChild){
-                    Node *child_ptr = (Node*)calloc(1,sizeof(Node));
-                    child_ptr->c = str[pos];
-                    ptr->children.push_back(child_ptr);
-                    ptr = ptr->children.back();
-                }
-                pos++;
             }
-            return ;
+            if (!isExistsChild) {
+                Node *child_ptr = (Node*)calloc(1,sizeof(Node));
+                child_ptr->c = str[pos];
+                ptr->children.push_back(child_ptr);
+                ptr = ptr->children.back();
+            }
+            pos++;
         }
+        return ;
+    }
 
-        bool isExists(string str){ // Trie に str に前方一致する文字列が登録されているかどうかを判定する
-            Node *ptr = &root; // 現在のノード
-            int pos = 0; // 文字列 str 上の位置
-            while(pos < str.size()){
-                bool isExistsChild = false;
-                for(int i = 0;i < (ptr->children).size(); i++){
-                    if(ptr->children[i]->c == str[pos]){
-                        isExistsChild = true;
-                        ptr = ptr->children[i];
-                        pos++;
-                        break;
-                    }
-                }
-                if(!isExistsChild){
-                    return false;
+    bool isExists(string str) { // Trie に str に前方一致する文字列が登録されているかどうかを判定する
+        Node *ptr = &root; // 現在のノード
+        int pos = 0; // 文字列 str 上の位置
+        while (pos < str.size()) {
+            bool isExistsChild = false;
+            for (int i = 0;i < (ptr->children).size(); i++) {
+                if (ptr->children[i]->c == str[pos]) {
+                    isExistsChild = true;
+                    ptr = ptr->children[i];
+                    pos++;
+                    break;
                 }
             }
-            return true;
+            if (!isExistsChild) {
+                return false;
+            }
         }
-    };
+        return true;
+    }
+};
+
+extern "C" {
+    Trie create_trie() {
+        return new Trie();
+    }
+
+    void insert(Trie *trie, string str) {
+        trie->insert(str);
+        return;
+    }
+
+    bool isExists(Trie *trie, string str) {
+        return trie->isExists(str);
+    }
 }
